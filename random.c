6d1f99414f73124d7c6131bd581b1756b2276831
mateuszSuchecki25 <mateusz.suchecki@temple.edu>
2025-09-01 22:43:39 -0400

    Update random.c

--- random.c
+++ random.c
@@ -1,37 +1,15 @@
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
-
-// random string generator wrapper function
-char* rand_string_alloc(size_t size)
+void rand_string(char *s, size_t size)
 {
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size); // to be developed by the student
-     }
-     return s;
-}
+    const char possibleChars[] =
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "0123456789";
 
-
-
-int main()
-
-{
-    char *str;
-    size_t size =   ;  //specify a value for size of word
-
-	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
-
-	printf("Today's random word: ");
-    str = rand_string_alloc(size );
-    printf("%s",str);
-    free (str);
-    
-
-	return(0);
+    if(size){
+        for (size_t n = 0; n < size; n++) {
+            int key = rand() % (int)(sizeof(possibleChars) - 1);
+            s[n] = possibleChars[key];
+        }
+        s[size] = '\0'; 
+    }
 }
